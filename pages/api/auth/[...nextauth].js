2f163669784fee4a30be208018d4fe15269fb5f9
Davin Suen <y6suen@gmail.com>
2021-06-03 16:37:34 -0400

    add scope options and return access token from spotify auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,9 +5,24 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Spotify({
+      scope: 'user-read-recently-played',
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      debug: true,
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async jwt(token, _, account) {
+      if (account) {
+        token.id = account.id;
+        token.accessToken = account.accessToken;
+      }
+      return token;
+    },
+    async session(session, user) {
+      session.user = user;
+      return session;
+    },
+  },
 });

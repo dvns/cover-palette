c0033b666aed149677a6fca1868620f8107fa2da
Davin Suen <y6suen@gmail.com>
2021-06-05 12:26:08 -0400

    syntax

--- pages/_app.js
+++ pages/_app.js
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }) {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css"
           integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
       </Head>
       <ThemeProvider theme={theme}>
